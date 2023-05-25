ef88707f8062398a8a4b4f7960607c3e16af139e
Mark <markedwards91@gmail.com>
2023-05-25 13:32:39 -0500

    is_empty

--- stack.py
+++ stack.py
@@ -1,5 +1,6 @@
 # Stack data structure could be described as stacking plates in your cubbard as you add to the top and remove from the top or the stack or first in first out. The oldest item in the stack would always be at the bottom and aways the last item to be removed. The newest item is the firt item to be removed.
 
+
 class Node:
     def __init__(self, data) -> None:
         self.data = data
@@ -9,8 +10,10 @@ class Stack:
     def __init__(self):
         self.head = None
 
-    def empty():
-        pass
+    def is_empty(self):
+        if self.head == None:
+            return True
+        return False
 
     def size():
         pass
