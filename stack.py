86419a1fc4519cf825d6189b9ccb2a1f9499ec94
Mark <markedwards91@gmail.com>
2023-05-25 13:26:14 -0500

    stack explanation.

--- stack.py
+++ stack.py
@@ -1,4 +1,4 @@
-
+# Stack data structure could be described as stacking plates in your cubbard as you add to the top and remove from the top or the stack or first in first out. The oldest item in the stack would always be at the bottom and aways the last item to be removed. The newest item is the firt item to be removed.
 
 class Node:
     def __init__(self, data) -> None:
@@ -11,16 +11,16 @@ class Stack:
 
     def empty():
         pass
-    
+
     def size():
         pass
-    
+
     def top():
         pass
-    
+
     def push(a):
         pass
-    
+
     def pop():
         pass
-    
+
