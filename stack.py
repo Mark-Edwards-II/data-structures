cfb1377c05c5ea59f4d179fcae9280d0ad00792d
Mark <markedwards91@gmail.com>
2023-05-25 13:18:03 -0500

    data-structures: stack as singly linked list

--- stack.py
+++ stack.py
@@ -1,1 +1,18 @@
+
+
+class Node:
+    def __init__(self, data) -> None:
+        self.data = data
+        self.next = None
+
 class Stack:
+    def empty():
+        pass
+    def size():
+        pass
+    def top():
+        pass
+    def push(a):
+        pass
+    def pop():
+        pass
